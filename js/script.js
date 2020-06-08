b2aba806b66fd048c7ad8a58dfe5b448991c5abd
Eslicdm <eslicdm1@gmail.com>
2020-06-08 10:02:51 -0300

    Criando Cobrinha, e alterando a variavel 2d

--- js/script.js
+++ js/script.js
@@ -1,13 +1,26 @@
 let canvas = document.getElementById("snake");
-let canvas = canvas.getContext("2d"); // renderiza o desenho do canvas e reconhece que é 2d
+let context = canvas.getContext("2d"); // renderiza o desenho do canvas e reconhece que é 2d
 let box = 32; // define o quadrado do canvas
+snake = [];
+snake[0] = {
+  x: 8 * box,
+  y: 8 * box
+}
 
 function criarBG() { // define o canvas
   context.fillStyle = "lightgreen"; // cor do canvas
   context.fillRect(0, 0, 16 * box, 16 * box); // Desenha o canvas. Posições: x, y, altura, largura
 }
 
+function criarCobrinha() { // cria a cobrinha
+  for(i = 0; i < snake.length; i++) { 
+    context.fillStyle = "green";
+    context.fillRect(snake[i].x, snake[i].y, box, box) // O fillRect cria a estrutura
+  }
+}
+
 // aplica as funções
 criarBG();
+criarCobrinha();
 
 let jogo = setInterval(iniciarJogo, 100);
